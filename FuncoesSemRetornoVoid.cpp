f1a11d136fd1eef62570bb521e40556f68251c2d
Bruno Daniel <brunoflemos441@icloud.com>
2024-02-07 00:05:50 -0400

    Primeiro Commit

---
+++ FuncoesSemRetornoVoid.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<stdio.h>
+#include<stdlib.h>
+
+using namespace std;
+
+//criando função
+void desenhaBorda() {
+    printf("----------------------");
+}
+
+int main()
+{
+    //chamando função
+    desenhaBorda();
+
+    printf("\nHi\n");
+
+    desenhaBorda();
+
+    return 0;
+
+}
+
